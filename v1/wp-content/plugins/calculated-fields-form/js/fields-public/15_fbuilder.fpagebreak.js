00ebfd8281ff2a05d7cd0b1a1d6a7911717a3100
Elvio Barbosa <elviobarbosa@gmail.com>
2026-04-21 16:04:40 -0300

    remove pasta do versionamento

--- v1/wp-content/plugins/calculated-fields-form/js/fields-public/15_fbuilder.fpagebreak.js
+++
@@ -1,9 +0,0 @@
-	$.fbuilder.controls['fPageBreak']=function(){};
-	$.extend(
-		$.fbuilder.controls['fPageBreak'].prototype,
-		$.fbuilder.controls['ffields'].prototype,
-		{
-			title:"Page Break",
-			ftype:"fPageBreak"
-		}
-	);
--- v1/wp-content/plugins/tablepress/admin/js/build/export.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '56eca2140404cb96d0dc');
